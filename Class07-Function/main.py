f3a822c06e11b56fd6424473ec388dffe782ddae
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-06-30 13:27:50 -0700

    learnning a new functions

--- Class07-Function/main.py
+++ Class07-Function/main.py
@@ -1,5 +1,87 @@
 def greet(name):
-    print (f"Hello {name}")
-
+    print (f"Hello {name}!")
+    print("How are you?")
 greet("Alice")
 greet("Bob")
+print('------------------------')
+
+def print_twice(x):
+    print(f"{x}")
+    print(f"{x}")
+print_twice("Hello")
+print('------------------------')
+
+def print_many(thing, n):
+    for _ in range(n):
+        print(thing)
+print_many("Hello",3)
+print('------------------------')
+
+def print_many(n, thing):
+    for _ in range(n):
+        print(thing)
+def print_twice(x):
+    print_many(2, x)
+print_twice("Hello")
+print('------------------------')
+
+def double(x):
+    return x * 2
+number = 5
+twice = double(number)
+print(number)
+print(twice)
+print('------------------------')
+
+def double(x):
+    return x * 2
+number = 5
+double(number)
+print(number)
+print('------------------------')
+
+def double(x):
+    return x * 2
+def quadruple(x):
+    return double(double(x))
+x = 5
+print(quadruple(x))
+print('------------------------')
+
+def assert_equal(actual, expected):
+    if actual == expected:
+        print("OK")
+    else:
+        print(f"Error! {repr(actual)} != {repr(expected)}")
+def double(x):
+    return x * 2
+assert_equal(double(2), 4)
+assert_equal(double(5), 10)
+print('------------------------')
+
+def assert_equal(actual, expected):
+    if actual == expected:
+        print("OK")
+    else:
+        print(f"Error! {repr(actual)} != {repr(expected)}")
+def double(x):
+    return x * 3
+assert_equal(double(2), 4)
+assert_equal(double(5), 10)
+print('------------------------')
+
+def assert_equal(actual, expected):
+    if actual == expected:
+        print("OK")
+    else:
+        print(f"Error! {repr(actual)} != {repr(expected)}")
+def double(x):
+    return x * 2
+def quadruple(x):
+    return double(double(x))
+assert_equal(quadruple(2), 8)
+assert_equal(quadruple(5), 20)
+print('------------------------')
+
+
+print('------------------------')
